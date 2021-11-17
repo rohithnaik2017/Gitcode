7a39ca70c93fc31352575dca4a70be1e352e2b56
rohithnaik2017 <rohithnaik2017@gmail.com>
2021-11-17 02:15:30 -0800

    Added print statement

--- Gitcode/ViewController.swift
+++ Gitcode/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello world")
+        
         // Do any additional setup after loading the view.
     }
 
